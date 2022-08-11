bc0549323106b4a23b08f2a324777c9a1d8c4edc
RomanGrishin <roman1988grishin@gmail.com>
2022-08-11 13:24:21 +0300

    draft consolse app

---
+++ Splitter/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+char separator = '|';
+
+string[] GetData(char separator, string dataKind)
+{
+    Console.WriteLine($"Give {dataKind}:");
+    string data = Console.ReadLine();
+    string[] strings = data.Split(separator);
+    Console.WriteLine($"Got {strings.Length} {dataKind}");
+    return strings;
+}
+
+string[] columns = GetData(separator, "columns");
+string[] values = GetData(separator, "values");
+
+for (int i = 0; i < columns.Length; i++)
+{
+    Console.WriteLine(columns[i] + ": " + values[i]);
+}
